7a7d1a363aa180212aff9723bff81b35bef20055
shadon016 <shadon016@gmail.com>
2025-03-11 23:43:24 +0600

    adding admin navigation

---
+++ app/(dashboard)/_components/adminNav.jsx
@@ -0,0 +1,34 @@
+import Logo from "@/components/logo.jsx";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+const AdminNav = () => {
+  return (
+    <div className="flex justify-between items-center wrapper">
+      <Logo />
+      <div>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Logout</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
+};
+
+export default AdminNav;
