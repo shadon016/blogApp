1a2d1e9490755b08837a3c809f9c6c8abadd88f6
shadon016 <shadon016@gmail.com>
2025-03-13 18:45:38 +0600

    created side nav

---
+++ app/(dashboard)/_components/sideNav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SideNav = () => {
+  return (
+    <div>
+      <Link href="">add post</Link>
+      <Link href="">all blog</Link>
+      <Link href="">settings</Link>
+    </div>
+  );
+};
+
+export default SideNav;
