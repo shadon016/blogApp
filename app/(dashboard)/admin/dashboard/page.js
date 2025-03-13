7ddb961b0cdd68f4dc07ed7f744b6061d2748648
shadon016 <shadon016@gmail.com>
2025-03-13 18:47:29 +0600

    minor changes

--- app/(dashboard)/admin/dashboard/page.js
+++ app/(dashboard)/admin/dashboard/page.js
@@ -1,12 +1,9 @@
-import { Button } from "@/components/ui/button.jsx";
+import SideNav from "../../_components/sideNav.jsx";
 
 export default function Dashboard() {
   return (
     <div className="m-4">
-      <h1>this is dashboard</h1>
-      <Button variant="destructive" className="bg-green-400 hover:bg-green-500">
-        click
-      </Button>
+      <SideNav />
     </div>
   );
 }
